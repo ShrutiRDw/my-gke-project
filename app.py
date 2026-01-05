01576e52d16d4e8330d9ba5fc57925a8df5c16db
ShrutiRDw <shrutiincolorcloudpalace@gmail.com>
2026-01-06 00:47:00 +0530

    Updating to Version 3

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.get('/')
 def hello():
-    return "<h1>DevOps Project Successfull to version 2 </h1><p> YAY Running on GKE Autopilot.</p>"
+    return "<h1>DevOps Project Successfull to version 3 </h1><p> YAY3 Running on GKE Autopilot.</p>"
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
